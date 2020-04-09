8fa65295df6f1368d4f7a535d93676f66d5d307c
Beta-y <58842921+Beta-y@users.noreply.github.com>
2020-04-09 11:10:54 +0800

    Update face_run.sh
    update user-warning

--- face_run.sh
+++ face_run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-rosrun face_detector kinect_img_publisher.py&rosrun face_detector turty_face_detection.py&rosrun face_detector main_loop.py 
 
+#本文件为一键运行，不推荐使用，因为关闭不当会导致摄像头端口持续占用
+rosrun face_detector kinect_img_publisher.py&rosrun face_detector turty_face_detection.py&rosrun face_detector main_loop.py&rosrun face_detector turty_face_recognition.py 
 
 #授予权限 chmod +x face_run.sh
